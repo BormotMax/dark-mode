19588db18cc6760ccc5681010ac1e34f4fc15aba
Matt <matthew.watts.mw@gmail.com>
2020-06-21 17:37:12 -0700

    Add Bulma styling library and support for SASS.
    And remove some code from create-next-app

---
+++ components/AboveTheFold/aboveTheFold.js
@@ -0,0 +1,9 @@
+import styles from "./AboveTheFold.module.scss"
+
+export function AboveTheFold() {
+  return (
+    <div className={styles["above-the-fold"]}>
+      <span className={styles.inner}>Continuum</span>
+    </div>
+  )
+}
