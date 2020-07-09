2a5a689f47fd8278c3f5fab358550533e01b7aa9
Matt <matthew.watts.mw@gmail.com>
2020-07-09 03:26:43 -0700

    Switch from amplify/api library to aws-appsync and react-apollo
    amplify/api wasn't working with nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,32 @@
 import '../styles.scss';
 import '../bulma.scss';
 import Amplify from 'aws-amplify';
+import { Auth } from '@aws-amplify/auth';
 import { AppProps } from 'next/app';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import { ApolloProvider } from 'react-apollo';
+import { Rehydrated } from 'aws-appsync-react';
 import awsconfig from '../aws-exports';
 
 Amplify.configure(awsconfig);
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const client = new AWSAppSyncClient({
+  url: awsconfig.aws_appsync_graphqlEndpoint,
+  region: awsconfig.aws_appsync_region,
+  disableOffline: true,
+  auth: {
+    type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+    jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken(),
+  },
+});
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </Rehydrated>
+  </ApolloProvider>
+);
 
 export default MyApp;
--- src/pages/project/[id].tsx
+++ src/pages/project/[id].tsx
@@ -1,10 +1,9 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { API, graphqlOperation } from 'aws-amplify';
-import { GraphQLResult } from '@aws-amplify/api';
+import gql from 'graphql-tag';
+import { Query, QueryResult } from 'react-apollo';
 import styles from '../styles/project.module.scss';
-import { data, files } from '../../mockData/project';
+import { data as mockData, files } from '../../mockData/project';
 import {
   TabGroup, NotesTab, QuotesTab, FilesTab,
 } from '../../components/tabs';
@@ -15,43 +14,11 @@ import { Footer } from '../../components/footer';
 import { ProjectHeader } from '../../components/projectHeader';
 import { QuoteProgress } from '../../components/quote';
 import { getProject } from '../../graphql/queries';
-import { GetProjectQuery } from '../../API';
 import { Project, Comment, Quote as QuoteType } from '../../types/custom';
 
 const ProjectPage: React.FC = () => {
-  const [project, setProject] = useState<Project>(null);
-  const [errors, setErrors] = useState(null);
   const router = useRouter();
   const { id } = router.query;
-
-  useEffect(() => {
-    const executeQuery = async () => {
-      try {
-        const result = (await API.graphql(graphqlOperation(getProject, { id }))) as GraphQLResult<GetProjectQuery>;
-        setProject(result.data?.getProject);
-      } catch (err) {
-        setErrors(err);
-      }
-    };
-
-    executeQuery();
-  }, []);
-
-  if (errors) {
-    return <div>There was an error</div>;
-  }
-
-  if (!project) {
-    return <div>Loading...</div>;
-  }
-
-  const {
-    initialContact, client, createdAt, comments: cs, quotes: qs,
-  } = project;
-
-  // todo: add sort order to quotes query
-  const quotes = (qs?.items || []) as Array<QuoteType>;
-  const comments = (cs?.items || []) as Array<Comment>;
 
   return (
     <div className={styles.page}>
@@ -59,31 +26,49 @@ const ProjectPage: React.FC = () => {
         <title>Continuum</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.body} container is-desktop`}>
-        <ProjectHeader headerText={data.company.name} avatar="/avatar.jpg" />
-        <div className={`${styles.columns} columns`}>
-          <div className={`${styles.leftColumn} column is-two-thirds-desktop`}>
-            <div className={styles.container}>
-              <FirstContact name={client?.name} message={initialContact?.message} submittedAt={new Date(createdAt)} />
-              <Comments comments={comments} />
-            </div>
-          </div>
-          <div className="column">
-            <div className={styles.container}>
-              <TabGroup names={['QUOTES', 'NOTES', 'FILES']}>
-                <QuotesTab quotes={quotes} />
-                <NotesTab />
-                <FilesTab files={files} />
-              </TabGroup>
-              <div className={styles.projectProgressHeader}>PROJECT PROGRESS</div>
-              {quotes.filter(Boolean).map((quote, i) => (
-                <QuoteProgress key={quote.id} i={i + 1} quote={quote} />
-              ))}
-            </div>
-          </div>
-        </div>
-      </main>
-      <Footer />
+      <Query query={gql(getProject)} variables={{ id }}>
+        {({ loading, data, error }: QueryResult) => {
+          if (loading) return <div>loading...</div>;
+          if (error) return <div>{error.message}</div>;
+
+          const {
+            initialContact, client, createdAt, comments: cs, quotes: qs,
+          } = data.getProject as Project;
+
+          const quotes = (qs?.items || []) as Array<QuoteType>;
+          const comments = (cs?.items || []) as Array<Comment>;
+
+          return (
+            <>
+              <main className={`${styles.body} container is-desktop`}>
+                <ProjectHeader headerText={mockData.company.name} avatar="/avatar.jpg" />
+                <div className={`${styles.columns} columns`}>
+                  <div className={`${styles.leftColumn} column is-two-thirds-desktop`}>
+                    <div className={styles.container}>
+                      <FirstContact name={client?.name} message={initialContact?.message} submittedAt={new Date(createdAt)} />
+                      <Comments comments={comments} />
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className={styles.container}>
+                      <TabGroup names={['QUOTES', 'NOTES', 'FILES']}>
+                        <QuotesTab quotes={quotes} />
+                        <NotesTab />
+                        <FilesTab files={files} />
+                      </TabGroup>
+                      <div className={styles.projectProgressHeader}>PROJECT PROGRESS</div>
+                      {quotes.filter(Boolean).map((quote, i) => (
+                        <QuoteProgress key={quote.id} i={i + 1} quote={quote} />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </main>
+              <Footer />
+            </>
+          );
+        }}
+      </Query>
     </div>
   );
 };
