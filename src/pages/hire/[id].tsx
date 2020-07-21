5019a3261f7cdccb0e2d245596e0625b73cf7f07
Matthew Watts <matthew.watts.mw@gmail.com>
2020-07-21 01:21:53 -0700

    Fetch HireMeInfo on hire page. (#8)
    And work on responsiveness

--- src/pages/hire/[id].tsx
+++ src/pages/hire/[id].tsx
@@ -1,9 +1,15 @@
 import styles from '../styles/hire.module.scss';
 import LinkedInLogo from '../../img/linkedIn.svg';
+import gql from 'graphql-tag';
 import InstagramLogo from '../../img/instagram.svg';
 import Dribbble from '../../img/dribbble.svg';
+import { useRouter } from 'next/router';
 import classnames from 'classnames';
-import { useState } from 'react';
+import { Storage } from 'aws-amplify';
+import { useState, useEffect, useContext } from 'react';
+import { HireMeInfoByFreelancerQuery } from '../../API';
+import { hireMeInfoByFreelancer } from '../../graphql/queries';
+import { ApolloContext } from 'react-apollo';
 
 const images = [
   '/portfolio_0.png',
@@ -22,62 +28,124 @@ enum Tab {
 }
 
 const Hire: React.FC = () => {
+  const router = useRouter();
+  const { id } = router.query;
   const [selectedTab, setSelectedTab] = useState(Tab.PORTFOLIO)
+  const [hireInfo, setHireInfo] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [portfolioImages, setPortfolioImages] = useState(null);
+  const [bannerImage, setBannerImage] = useState(null);
+  const { client } = useContext(ApolloContext);
 
+  useEffect(() => {
+    const execute = async () => {
+      if(!id) return;
+      try {
+        const { data }: {data: HireMeInfoByFreelancerQuery} = await client.query({
+          query: gql(hireMeInfoByFreelancer),
+          variables: { freelancerID: id },
+        });
+
+        const info = data?.hireMeInfoByFreelancer?.items[0];
+
+        if (info) {
+          const map = {};
+          const promises = [];
+
+          info.portfolioImages?.forEach(({ key, tag }) => {
+            try {
+              promises.push(Storage.get(key).then((img) => map[tag] = img));
+            } catch (err) {
+              console.log(err);
+            }
+          });
+
+          Promise.all(promises).then(() => setPortfolioImages(map))
+
+          if (info.bannerImage) {
+            try {
+              Storage.get(info.bannerImage.key).then((b) => setBannerImage(b));
+            } catch (err) {
+              console.log(err);
+            }
+          }
+        }
+
+        setHireInfo(info);
+      } catch (err) {
+        console.log(err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    execute();
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>;
+  if (!hireInfo) return <div>There is no hire page here, yet.</div>;
+
+  const { name, title, buttonText, blurbText, aboutText } = hireInfo;
   return (
-  <div className={styles.hire}>
-    <div className={styles.upper}>
-      <div className={classnames(styles.left, "is-block-fullhd")}>
-        <div className={classnames("text-small-caps", styles.name)}>NAOMI PRICE</div>
-        <div className={classnames(styles.title, "h1")}>Freelance Illustrator</div>
-        <div className={styles.blurbText}>I am an illustrator by education and training. I studied fine art and art history at Boston University then went on to.</div>
-        <div className="tar">
-          <button className={styles.button} type="button">Start A Conversation</button>
+    <div className={styles.hire}>
+      <div className={styles.upper}>
+        <div className={classnames(styles.leftContainer, "is-block-fullhd")}>
+          <div className={styles.left}>
+          <div className={classnames("text-small-caps", styles.name)}>{name}</div>
+          <div className={classnames(styles.title, "h1")}>{title}</div>
+          <div className={styles.blurbText}>{blurbText}</div>
+          <div className="tar">
+            <button className={styles.button} type="button">{buttonText}</button>
+          </div>
+          </div>
+        </div>
+        <div className={styles.bannerImageContainer}>
+          {
+            bannerImage && <img className={styles.bannerImage} src={bannerImage} />
+          }
         </div>
       </div>
-      <img className={styles.bannerImage} src="/hire2.png" />
-    </div>
-    <div className="container is-fullhd">
-      <div className={classnames("text-small-caps", styles.optionsBar)}>
-        <div onClick={() => setSelectedTab(Tab.PORTFOLIO)} className={classnames({[styles.selected]: selectedTab === Tab.PORTFOLIO})}>Portfolio</div>
-        <div onClick={() => setSelectedTab(Tab.ABOUT)} className={classnames({[styles.selected]: selectedTab === Tab.ABOUT}, "mlxl")}>About</div>
-      </div>
-      {
-        selectedTab === Tab.PORTFOLIO &&
-       <div className={styles.portfolioImages}>
-          {images.map((image) => <div key={image} className={styles.portfolioImage}><img src={image} alt="portfolio" /></div>)}
+      <div className="container is-fullhd">
+        <div className={classnames("text-small-caps", styles.optionsBar)}>
+          <div onClick={() => setSelectedTab(Tab.PORTFOLIO)} className={classnames({[styles.selected]: selectedTab === Tab.PORTFOLIO})}>Portfolio</div>
+          <div onClick={() => setSelectedTab(Tab.ABOUT)} className={classnames({[styles.selected]: selectedTab === Tab.ABOUT}, "mlxl")}>About</div>
         </div>
-      }
-      {
-       selectedTab === Tab.ABOUT &&
-       <div className={classnames(styles.about)}>
-         {`Hello, I am Naomi, and I live and breathe illustration.
-
-I am an illustrator by education and training. I studied fine art history at Boston University then went on to receive my MFA in illustration, with distinction, from Yale University.
-
-I have participated in 24 gallery shows and 9 solo exihibits at distinguished institutions such as the Musee du Louvre, The Metropolitan Museum of Art and the Boston Museum of Fine Arts.
-
-My illustrations have been featured in Vogue, Bazaar, Marie Claire and dozens more magazines. I have had the privilege of working for distinguished clients such as Tiffany, Van Cleef and Arples, Chanel, Acne and Estee Lauder.
-
-I am currenly accepting new engagements.`}
-       </div> 
-      }
-    </div>
-    <div className={styles.footer}>
-      <div>
-        <InstagramLogo />
-        <LinkedInLogo />
-        <Dribbble />
+        {selectedTab === Tab.PORTFOLIO && !portfolioImages && <div className={styles.imageLoader}></div> }
+        {
+          selectedTab === Tab.PORTFOLIO && portfolioImages &&
+            <div className={styles.portfolioImages}>
+              <div className={styles.portfolioImage}><img src={portfolioImages["portfolio-1"]} alt="portfolio" /></div>
+              <div className={styles.portfolioImage}><img src={portfolioImages["portfolio-2"]} alt="portfolio" /></div>
+              <div className={styles.portfolioImage}><img src={portfolioImages["portfolio-3"]} alt="portfolio" /></div>
+              <div className={styles.portfolioImage}><img src={portfolioImages["portfolio-4"]} alt="portfolio" /></div>
+              <div className={styles.portfolioImage}><img src={portfolioImages["portfolio-5"]} alt="portfolio" /></div>
+              <div className={styles.portfolioImage}><img src={portfolioImages["portfolio-6"]} alt="portfolio" /></div>
+            </div>
+        }
+        {
+         selectedTab === Tab.ABOUT &&
+         <div className={classnames(styles.about)}>
+           {aboutText}
+         </div> 
+        }
       </div>
-      <div className={styles.copyright}>
-        &copy;
-        {' '}
-        {new Date().getFullYear()}
-        {' '}
-        {name}
+      <div className={styles.footer}>
+        <div>
+          <InstagramLogo />
+          <LinkedInLogo />
+          <Dribbble />
+        </div>
+        <div className={styles.copyright}>
+          &copy;
+          {' '}
+          {new Date().getFullYear()}
+          {' '}
+          {name}
+        </div>
       </div>
     </div>
-  </div>
-)};
+  )
+};
 
 export default Hire;
