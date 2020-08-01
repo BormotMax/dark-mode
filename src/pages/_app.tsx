815e22726a5fb15e4dc649ef18c3f198612bdb74
Matt <matthew.watts.mw@gmail.com>
2020-08-01 03:14:00 -0700

    Add both AWS Pinpoint and Google analytics to try them out.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/no-danger */
+/* eslint-disable react/no-unescaped-entities */
 import '../styles.scss';
 import '../bulma.scss';
 import Amplify from 'aws-amplify';
 import { Auth } from '@aws-amplify/auth';
 import { AppProps } from 'next/app';
 import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import Analytics from '@aws-amplify/analytics';
 import { ApolloProvider } from 'react-apollo';
 import { Rehydrated } from 'aws-appsync-react';
+import Head from 'next/head';
 import awsconfig from '../aws-exports';
 import { UserDataProvider } from '../hooks/useCurrentUser';
 
 Amplify.configure(awsconfig);
+
+Analytics.autoTrack('pageView', {
+  enable: true,
+  // you need to change it to 'SPA' if your app is a single-page app like React
+  type: 'multiPageApp',
+  getUrl: () => window.location.origin + window.location.pathname,
+});
 
 export const client = new AWSAppSyncClient({
   url: awsconfig.aws_appsync_graphqlEndpoint,
@@ -32,6 +43,19 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <UserDataProvider>
     <ApolloProvider client={client}>
       <Rehydrated>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-174215284-1" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-174215284-1');
+`,
+            }}
+          />
+        </Head>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </Rehydrated>
