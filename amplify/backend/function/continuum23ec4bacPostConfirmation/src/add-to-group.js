4e94b3afc3a5e30f097250bba3114ec251996481
Matt <matthew.watts.mw@gmail.com>
2020-09-05 00:30:44 -0700

    Create user in dynamo in post confirmation lambda

--- amplify/backend/function/continuum23ec4bacPostConfirmation/src/add-to-group.js
+++ amplify/backend/function/continuum23ec4bacPostConfirmation/src/add-to-group.js
@@ -2,30 +2,65 @@
 
 exports.handler = async (event, context, callback) => {
   const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
+  const ddb = new aws.DynamoDB({ apiVersion: '2012-10-08' });
+
+  const tableName = process.env.TABLE_NAME;
+  const region = process.env.REGION;
+
+  aws.config.update({ region: region });
 
   const group = 'FREELANCER';
 
-  const groupParams = {
-    GroupName: group,
-    UserPoolId: event.userPoolId,
-  };
+  if (event.request.userAttributes.sub) {
+    let date = new Date();
 
-  const addUserParams = {
-    GroupName: group,
-    UserPoolId: event.userPoolId,
-    Username: event.userName,
-  };
+    // -- Write data to DDB
+    let ddbParams = {
+      Item: {
+        id: { S: event.request.userAttributes.sub },
+        __typename: { S: 'User' },
+        owner: { S: event.userName },
+        email: { S: event.request.userAttributes.email },
+        createdAt: { S: date.toISOString() },
+        updatedAt: { S: date.toISOString() },
+        role: { S: 'FREELANCER' },
+      },
+      TableName: tableName,
+    };
 
-  try {
-    await cognitoidentityserviceprovider.getGroup(groupParams).promise();
-  } catch (e) {
-    await cognitoidentityserviceprovider.createGroup(groupParams).promise();
-  }
+    // Call DynamoDB
+    try {
+      await ddb.putItem(ddbParams).promise();
+      console.log('Success');
+    } catch (err) {
+      console.log('Error', err);
+    }
 
-  try {
-    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
+    const groupParams = {
+      GroupName: group,
+      UserPoolId: event.userPoolId,
+    };
+
+    const addUserParams = {
+      GroupName: group,
+      UserPoolId: event.userPoolId,
+      Username: event.userName,
+    };
+
+    try {
+      await cognitoidentityserviceprovider.getGroup(groupParams).promise();
+    } catch (e) {
+      await cognitoidentityserviceprovider.createGroup(groupParams).promise();
+    }
+
+    try {
+      await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
+      callback(null, event);
+    } catch (e) {
+      callback(e);
+    }
+  } else {
+    console.log('no email in event');
     callback(null, event);
-  } catch (e) {
-    callback(e);
   }
 };
