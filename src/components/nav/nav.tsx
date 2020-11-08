25642e86734d1297e2b6d06c1b8139e7164d2667
Matthew Watts <matthew.watts.mw@gmail.com>
2020-11-07 17:47:02 -0800

    Add AllUsersPage (#44)

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -14,6 +14,7 @@ export enum Page {
   PROJECTS,
   HIRE,
   HIRE_EDITOR,
+  ALL_USERS,
 }
 
 interface NavProps {
---
+++ src/pages/all-users.tsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import gql from 'graphql-tag';
+import classnames from 'classnames';
+import { WithAuthentication, RouteType, Role } from '../components/withAuthentication';
+import { client } from './_app';
+import { ListUsersQuery } from '../API';
+import { useFlash, useLogger } from '../hooks';
+import { PageLayoutOne } from '../components/pageLayoutOne';
+import { Page } from '../components/nav/nav';
+import { listUsers } from '../graphql/queries';
+import { User } from '../types/custom';
+import styles from './styles/allUsers.module.scss';
+
+const AllUsersPage: React.FC = () => {
+  const [users, setUsers] = useState([]);
+  const { setFlash } = useFlash();
+  const { logger } = useLogger();
+
+  const fetchUsers = async () => {
+    try {
+      const listUsersResult: { data: ListUsersQuery } = await client.query({ query: gql(listUsers) });
+
+      const p: User[] = listUsersResult.data?.listUsers?.items || [];
+      setUsers(p);
+    } catch (error) {
+      setFlash("There was an error retrieving the users. We're looking into it.");
+      logger.error('AllUsersPage: error retrieving users.', { error });
+    }
+  };
+
+  useEffect(() => {
+    const execute = async () => {
+      fetchUsers();
+    };
+    execute();
+  }, []);
+
+  const copyEmails = () => {
+    const tempInput = document.createElement('input');
+    tempInput.value = users
+      .filter(Boolean)
+      .filter((user) => user.role === Role.FREELANCER)
+      .map((user) => user.email)
+      .join(',');
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+  };
+
+  return (
+    <PageLayoutOne
+      headerText={
+        <>
+          All Users - <small>Admin Only</small>
+        </>
+      }
+      page={Page.ALL_USERS}
+    >
+      <div className={classnames('column', 'is-narrow', styles.allUsers)}>
+        <button type="button" onClick={copyEmails} className="button">
+          Copy freelancer emails to clipboard
+        </button>
+        <h2>Freelancer Accounts</h2>
+        <div className={classnames(styles.section)}>
+          {users
+            .filter(Boolean)
+            .filter((user) => user.role === Role.FREELANCER)
+            .map((user) => (
+              <Card key={user.id} user={user} />
+            ))}
+        </div>
+        <h2>Clients</h2>
+        {users
+          .filter(Boolean)
+          .filter((user) => user.role !== Role.FREELANCER)
+          .map((user) => (
+            <Card key={user.id} user={user} />
+          ))}
+      </div>
+    </PageLayoutOne>
+  );
+};
+
+const Card = ({ user }) => (
+  <div className={classnames('card', styles.card)}>
+    <div className={classnames(styles.cardContent, 'card-content')}>
+      <div>
+        <p>
+          <b>Name:</b> {user.name}
+        </p>
+        <p>
+          <b>Title:</b> {user.title}
+        </p>
+        <p>
+          <b>Email:</b> {user.email}
+        </p>
+      </div>
+      <div>
+        <p>
+          <b>Phone:</b> {user.phone}
+        </p>
+        <p>
+          <b>Role:</b> {user.role}
+        </p>
+        <p>
+          <b>Created At:</b> {new Date(user.createdAt).toLocaleString()}
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default WithAuthentication(AllUsersPage, { routeType: RouteType.SIGNED_IN, allowedRoles: [Role.ADMIN] });
