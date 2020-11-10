fd824af4e468a7437b9d1f877c53579dc5ae3c9e
ekreerenko <evgeniykreerenko@dunice.net>
2020-11-10 18:52:06 +0300

    add accept/decline buttons layout

--- src/components/comment/quoteForComment/quoteForComment.tsx
+++ src/components/comment/quoteForComment/quoteForComment.tsx
@@ -3,12 +3,14 @@ import classnames from 'classnames';
 import gql from 'graphql-tag';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/pro-regular-svg-icons';
+import { Protected } from '../../protected/protected';
 import { useLogger } from '../../../hooks';
 import { getQuote } from '../../../graphql/queries';
 import { GetQuoteQuery } from '../../../API';
 import { unauthClient as client } from '../../../pages/_app';
 import styles from './quoteForComment.module.scss';
 import { Quote } from '../../../types/custom';
+import { Role } from '../../withAuthentication';
 
 interface QuoteForCommentProps {
   id: string;
@@ -37,6 +39,7 @@ export const QuoteForComment: React.FC<QuoteForCommentProps> = ({ id }) => {
     executeGetQuote();
   }, []);
 
+  console.log('quote -->', quote);
   return !quote ? null : (
     <div className={classnames(styles.quoteForComment)}>
       <div className={classnames(styles.header)}>
@@ -52,6 +55,29 @@ export const QuoteForComment: React.FC<QuoteForCommentProps> = ({ id }) => {
               <div>{task.text}</div>
             </div>
           ))}
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <Protected roles={[Role.CLIENT]}>
+          <button
+            form="hirePageForm"
+            disabled={false}
+            type="submit"
+            className={classnames('btn-large', 'btn-large--inline', 'button', { 'is-loading': false })}
+          >
+            Accept & Pay $6000 Deposit
+          </button>
+        </Protected>
+        <a href="#" className={styles.declineStyles}>
+          Decline
+        </a>
+        {/* <button */}
+        {/*  form="hirePageForm" */}
+        {/*  disabled={false} */}
+        {/*  className={classnames('btn-small', 'btn-invert')} */}
+        {/* > */}
+        {/*  Decline */}
+        {/* </button */}
       </div>
     </div>
   );
