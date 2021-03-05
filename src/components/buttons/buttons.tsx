1300440cfddf3f96e3686c2517ebc86a6b0e6efb
bormot <bormotmax@gmail.com>
2021-03-05 14:59:38 +0200

    added Confirm and Delete buttons

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -1,7 +1,15 @@
 import React, { useMemo, memo } from 'react';
 import classnames from 'classnames';
+import { faTrash } from '@fortawesome/pro-light-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './buttons.module.scss';
+
+export enum Size {
+  SMALL,
+  MIDDLE,
+  BIG
+}
 
 interface ButtonProps {
   text: string;
@@ -58,3 +66,79 @@ export const ButtonSmall: React.FC<ButtonProps> = memo(({
 });
 
 ButtonSmall.displayName = 'ButtonSmall';
+
+export const ConfirmButton: React.FC<ButtonProps> = memo(({
+  text,
+  isSaving,
+  onClick,
+  inverted = false,
+  form,
+  disabled,
+  className,
+  extraBorderRadius = false,
+  size,
+  invert,
+}) => {
+  const fontSize = size === Size.MIDDLE ? styles.confirmButton__middle : styles.confirmButton;
+  const invertColor = invert ? styles.confirmButton__invert : '';
+
+  return (
+    <button
+      onClick={onClick || undefined}
+      form={form}
+      disabled={isSaving || disabled}
+      type="submit"
+      className={classnames(
+        styles.button,
+        fontSize,
+        className,
+        invertColor,
+        {
+          [styles.loading]: isSaving && !disabled,
+          [styles.disabled]: disabled && !isSaving,
+          [styles.inverted]: inverted,
+          [styles.extraBorderRadius]: extraBorderRadius,
+        },
+      )}
+    >
+      {text}
+    </button>
+  );
+});
+
+ConfirmButton.displayName = 'ConfirmButton';
+
+export const DeleteButton: React.FC<ButtonProps> = memo(({
+  text,
+  isSaving,
+  onClick,
+  inverted = false,
+  disabled,
+  className,
+  extraBorderRadius = false,
+}) => {
+
+  return (
+    <button
+      onClick={onClick || undefined}
+      disabled={isSaving || disabled}
+      type="button"
+      className={classnames(
+        styles.button,
+        styles.deleteButton,
+        className,
+        {
+          [styles.loading]: isSaving && !disabled,
+          [styles.disabled]: disabled && !isSaving,
+          [styles.inverted]: inverted,
+          [styles.extraBorderRadius]: extraBorderRadius,
+        },
+      )}
+    >
+      <FontAwesomeIcon color="white" icon={faTrash} />
+      {text}
+    </button>
+  );
+});
+
+DeleteButton.displayName = 'DeleteButton';
